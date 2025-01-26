04f331e0395680174d2ad3dce627d5080ed72a47
Vishalpandey328 <vishalpandey328@gmail.com>
2025-01-26 18:22:03 +0530

    Update loan.py

--- loan.py
+++ loan.py
@@ -6,19 +6,33 @@ app = Flask(__name__)
 model_file = open("classifier.pkl", "rb")
 model = pickle.load(model_file)
 
-
 # model.predict(inputs)
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Loan Approval Application</h1>"
 
-
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        # use model and predict
-
-        pass
+        loan_req = request.get_json()
+        if loan_req['Gender']=='Male':
+            Gender = 0
+        else:
+            Gender = 1
+        if loan_req['Married']=='Unmerried':
+            Married = 0
+        else:
+            Married = 1
+        ApplicantIncome = loan_req['ApplicantIncome']
+        LoanAmount = loan_req['LoanAmount']
+        Credit_History = loan_req['Credit_History']
+        result = model.predict([[Gender,Married,ApplicantIncome,LoanAmount,Credit_History]])   
+        
+        if result == 0:
+            pred = "Rejected"
+        else:
+            pred = "Approved"        
+        return {"Loan_approval_status":pred}
     else:
         return "I Will Predict"
